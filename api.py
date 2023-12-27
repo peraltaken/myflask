f7f7ce2e8992c7162e58ee379ba00aa99d4e4ae9
snoopsksk <peraltaken09@gmail.com>
2023-12-27 13:24:51 +0800

    SQL CONFIG

---
+++ api.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "root"
+app.config["MYSQL_DB"] = "mydb"
+
+
+mysql = MySQL(app)
